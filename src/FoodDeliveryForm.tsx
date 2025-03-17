f7b70f9f7e32af80bd25c75278ebeb70e2d57a03
Erica Basak <ericabasak@gmail.com>
2025-03-17 14:43:53 -0700

    two input form with react only

---
+++ src/FoodDeliveryForm.tsx
@@ -0,0 +1,64 @@
+import { ChangeEvent, SyntheticEvent, useState } from 'react'
+
+
+type FoodDeliveryFormType = {
+    customerName: string;
+    phone: string;
+}
+
+type FoodDeliveryFormErrorType = {
+    customerName: string;
+    phone: string;
+}
+
+export const FoodDeliveryForm = () => {
+    const [values, setValues] = useState<FoodDeliveryFormType>({
+        customerName: "",
+        phone: ""
+    });
+
+    const [errors, setErrors] = useState<FoodDeliveryFormErrorType>({
+        customerName: "",
+        phone: "",
+    })
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setValues({ ...values, [name]: value });
+    }
+
+    const validateFormData = () => {
+        let tempErrors: FoodDeliveryFormErrorType = {
+            customerName: "",
+            phone: "",
+        }
+        if (values.customerName === "")
+        tempErrors.customerName = "Customer namer is required";
+        if (values.phone === "")
+        tempErrors.phone = "Phone number is required";
+        setErrors(tempErrors);
+
+        return Object.values(errors).every(x => x == "")
+    }
+
+    const onSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(values);
+
+        if(validateFormData())
+        console.log('form data ', values)
+        else console.log('form is invalid')
+
+    }
+  return (
+    <form autoComplete="off" onSubmit={onSubmit}>
+        <input name="customerName" type="text" placeholder='Customer Name' value={values.customerName} onChange={handleInputChange}/>
+        <label>Customer Name</label>
+        <input name="Phone" type="text" placeholder='Phone' value={values.phone} onChange={handleInputChange} />
+        <label>Phone Number</label>
+        <button className="btn btn-primary" type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default FoodDeliveryForm
