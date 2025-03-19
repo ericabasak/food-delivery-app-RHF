57d22cfe16cde56ef1ca6268f36bf39c771d3adb
Erica Basak <ericabasak@gmail.com>
2025-03-19 15:10:50 -0700

    react hook form with register and handlesubmit

--- src/FoodDeliveryForm.tsx
+++ src/FoodDeliveryForm.tsx
@@ -1,32 +1,57 @@
 import { useForm } from "react-hook-form";
-
+import { useRenderCount } from "./utils/useRenderCount";
 
 type FoodDeliveryFormType = {
+    orderNo: number;
     customerName: string;
     phone: string;
+    email: string;
 }
+
+const RenderCount = useRenderCount();
 
 export const FoodDeliveryForm = () => {
 
-    const { register, handleSubmit } = useForm<FoodDeliveryFormType>();
+    const { register, handleSubmit } = useForm<FoodDeliveryFormType>({
+        defaultValues: {
+            orderNo: new Date().valueOf(),
+            customerName: "",
+            phone: "",
+            email: "",
+        }
+    });
 
-    const onSubmit = (formData: FoodDeliveryFormType) => { console.log(formData)
-    }
+    const onSubmit = (formData: FoodDeliveryFormType) => { console.log(formData)}
 
   return (
     <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+        <RenderCount />
         <input 
         type="text" 
         placeholder='Customer Name'
-        {...register("customerName", { value: "Erica"})}
+        {...register("customerName")}
         />
         <label>Customer Name</label>
         <input 
         type="text" 
         placeholder='phone'
-        {...register("phone", { required: "Phone number is required"})} 
+        {...register("phone")} 
         />
         <label>Phone Number</label>
+        <input
+        type="text"
+        placeholder="Order Number"
+        {...register("orderNo")}
+        
+        />
+        <label>Order Number</label>
+        <input
+        type="text"
+        placeholder="email"
+        disabled
+        {...register("email")}
+        />
+        <label>Email</label>
         <button className="btn btn-primary" type="submit">Submit</button>
     </form>
   )
